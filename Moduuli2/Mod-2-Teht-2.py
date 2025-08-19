846cf217977b680887d3b2b92da07c33b047679a
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-08-19 18:06:34 +0300

    Update

--- Moduuli2/Mod-2-Teht-2.py
+++ Moduuli2/Mod-2-Teht-2.py
@@ -4,4 +4,4 @@ circle_r_str = input('Anna ympyrän säde: ')
 circle_r = float(circle_r_str)
 
 circle_area = math.pi * float(circle_r) ** 2
-print = (f'Ympyrän alue on: {circle_area:.2f}')
+print(f'Ympyrän alue on: {circle_area:.2f}.')
