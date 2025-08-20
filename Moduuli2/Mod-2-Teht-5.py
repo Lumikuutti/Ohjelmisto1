11dd220150d9492877cdc129cdcaa869bdfe76ca
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-08-20 13:43:12 +0300

    Update

--- Moduuli2/Mod-2-Teht-5.py
+++ Moduuli2/Mod-2-Teht-5.py
@@ -8,6 +8,10 @@ naulat_luoteina = (naulat + leiviskat_nauloina)*32
 
 luodit_str = input('Anna luodit: ')
 luodit = float(luodit_str)
-luodit_grammoina = (naulat_luoteina + luodit)*13.3
+yht_paino = (naulat_luoteina + luodit)*13.3
 
-print(f'Paino grammoina: {luodit_grammoina:.2f}')
+luodit_kg = yht_paino // 1000
+luodit_g = yht_paino % 1000
+
+print('Paino on nykyaikaisin mitoin:')
+print(f'{luodit_kg:.0f} kilogrammaa ja {luodit_g:.0f} grammaa.')
