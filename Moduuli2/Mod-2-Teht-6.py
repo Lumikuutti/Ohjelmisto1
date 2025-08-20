9bd5946b65a8479ed30c8ab648abe0adfad35ee2
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-08-20 13:58:00 +0300

    Update

---
+++ Moduuli2/Mod-2-Teht-6.py
@@ -0,0 +1,13 @@
+import random
+
+code_3a = random.randint(0, 9)
+code_3b = random.randint(0, 9)
+code_3c = random.randint(0, 9)
+
+code_4a = random.randint(1, 6)
+code_4b = random.randint(1, 6)
+code_4c = random.randint(1, 6)
+code_4d = random.randint(1, 6)
+
+print(f'Random kolminumeroinen koodisi on: {code_3a, code_3b, code_3c}')
+print(f'Random nelinumeroinen koodisi on: {code_4a, code_4b, code_4c, code_4d}')
