6f7cef22ae0cc94c99d8536a7e7d7b818fd821b8
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-08-26 10:56:06 +0300

    Update

--- Moduuli3/tuntitehtävä2.py
+++ Moduuli3/tuntitehtävä2.py
@@ -2,7 +2,9 @@
 used_hours = float( input("Anna kuinka monta kilowatti tuntia olet kuluttanut numerona: "))
 
 #lasketaan sähkölasku
-if 0 > used_hours <= 50:
+if used_hours < 0:
+    print("ERROR: Annettua arvoa ei voi käyttää.")
+elif 0 >= used_hours <= 50:
     laskutus = used_hours * 0.10
     print(f"Sähkölaskusi on {laskutus:.2f} euroa.")
 elif 50 > used_hours <= 200:
@@ -12,4 +14,4 @@ elif used_hours > 200:
     laskutus = 50 * 0.10 + 150 * 0.8 + (used_hours - 200) * 0.6
     print(f"Sähkölaskusi on {laskutus:.2f} euroa.")
 else:
-    print("ERROR: Annettua arvoa ei voi käyttää.")
+    print("ERROR: Et antanut systeemin ymmärtävää numeroa.")
