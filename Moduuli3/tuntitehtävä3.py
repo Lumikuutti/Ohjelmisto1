33fac5115033e8cc815db96c56ac6df26e25898b
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-08-26 11:19:44 +0300

    Update

--- Moduuli3/tuntitehtävä3.py
+++ Moduuli3/tuntitehtävä3.py
@@ -1,11 +1,11 @@
 #kysytään matikan, fysiikan ja kemian tulokset
-math_result = float( input("Ilmoita matematiikan tulos: "))
-physics_result = float( input("Ilmoita fysiikan tulos: "))
-chem_result = float( input("Ilmoita kemian tulos: "))
+math_result = int( input("Ilmoita matematiikan tulos: "))
+physics_result = int( input("Ilmoita fysiikan tulos: "))
+chem_result = int( input("Ilmoita kemian tulos: "))
 
-if math_result or physics_result or chem_result < 50:
+if (math_result or physics_result or chem_result) < 50:
     print("Et voi saada stipendiä, jokin tuloksistasi oli alle 50.")
-elif math_result and physics_result >= 90 or chem_result >= 95:
-    (print("Sait stipending, onneksi olkoon!"))
+elif (math_result and physics_result) >= 90 or chem_result >= 95:
+    (print("Sait stipendin, onneksi olkoon!"))
 else:
     print("Et saanut ikävä kyllä stipendiä.")
