7546e35316f1fe18aff549ea0ed84e1f389d1bf9
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-08-26 11:32:33 +0300

    Update

--- Moduuli3/tuntitehtävä3.py
+++ Moduuli3/tuntitehtävä3.py
@@ -5,7 +5,7 @@ chem_result = int( input("Ilmoita kemian tulos: "))
 
 if (math_result or physics_result or chem_result) < 50:
     print("Et voi saada stipendiä, jokin tuloksistasi oli alle 50.")
-elif (math_result and physics_result) >= 90 or chem_result >= 95:
+elif (math_result and physics_result) > 90 or chem_result > 95:
     (print("Sait stipendin, onneksi olkoon!"))
 else:
     print("Et saanut ikävä kyllä stipendiä.")
