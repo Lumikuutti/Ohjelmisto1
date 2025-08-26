6e9a1bac1267eb7201f6c8972de1e0fcf5a564a7
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-08-26 10:42:22 +0300

    Update

--- Moduuli3/tuntitehtävä2.py
+++ Moduuli3/tuntitehtävä2.py
@@ -1,13 +1,13 @@
 used_hours = float( input("Anna kuinka monta kilowatti tuntia olet kuluttanut numerona: "))
 
-if used_hours <= 50:
-    laskutus = used_hours * 10
-    print(f"Sähkölaskusi on {laskutus:.2f} senttiä.")
-elif 50 > used_hours < 200:
-    laskutus = 50 * 10 + (used_hours - 50) * 8
-    print(f"Sähkölaskusi on {laskutus:.2f} senttiä.")
-elif 150 > used_hours:
-    laskutus = 50 * 10 + 150 * 8 + (used_hours - 200) * 6
-    print(f"Sähkölaskusi on {laskutus:.2f} senttiä.")
+if 0 > used_hours <= 50:
+    laskutus = used_hours * 0.10
+    print(f"Sähkölaskusi on {laskutus:.2f} euroa.")
+elif 50 > used_hours <= 200:
+    laskutus = 50 * 0.10 + (used_hours - 50) * 0.8
+    print(f"Sähkölaskusi on {laskutus:.2f} euroa.")
+elif used_hours > 200:
+    laskutus = 50 * 0.10 + 150 * 0.8 + (used_hours - 200) * 0.6
+    print(f"Sähkölaskusi on {laskutus:.2f} euroa.")
 else:
     print("ERROR: Annettua arvoa ei voi käyttää.")
