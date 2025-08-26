e3fa347cc7d56be7ed785773283a4ce3e3a9234b
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-08-26 18:36:33 +0300

    Update

---
+++ Moduuli3/Mod-3-Teht-3.py
@@ -0,0 +1,19 @@
+gender = input("Ilmoita sukupuolesi (AFAB tai AMAB): ")
+hemo_glob = float( input("Ilmoita hemoglobiiniarvosi (g/l): "))
+
+if gender == "AFAB":
+    if hemo_glob < 117:
+        print("Hemoglobiiniarvosi on alhainen.")
+    elif hemo_glob < 175:
+        print("Hemoglobiiniarvosi on normaali.")
+    else:
+        print("Hemoglobiiniarvosi on korkea.")
+elif gender == "AMAB":
+    if hemo_glob < 134:
+        print("Hemoglobiiniarvosi on alhainen.")
+    elif hemo_glob < 195:
+        print("Hemoglobiiniarvosi on normaali.")
+    else:
+        print("Hemoglobiiniarvosi on korkea.")
+else:
+    print("Jotain meni vikaan.")
