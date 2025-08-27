8f497010a0a3d7979cb6aafab79b4814f752e68d
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-08-27 22:35:18 +0300

    Update

--- Moduuli4/Mod-4-Teht-6.py
+++ Moduuli4/Mod-4-Teht-6.py
@@ -6,8 +6,8 @@ points_in_circle = 0
 points_to_generate = int( input("Anna kuinka monta pistettä kokeillaan: "))
 
 while total_points < points_to_generate:
-    x = random.randrange (-1, 1)
-    y = random.randrange (-1, 1)
+    x = random.uniform (-1,1)
+    y = random.uniform (-1, 1)
     total_points = total_points + 1
     if (x ** 2 + y ** 2) < 1:
         points_in_circle = points_in_circle +1
@@ -16,6 +16,5 @@ if points_to_generate <= 0:
     print("En voi laskea piin likimäärää negativiisesta määrästä lähtien tai ilman pisteitä ollenkaan.")
 
 else:
-    pii = 4 * points_in_circle / total_points
-    print (f"Piin likimääräinen arvo Monte Carlo methodille on {pii:.4f}, {points_to_generate} genoroidun pisteen perusteella.")
-
+    pii = (4 * points_in_circle) / total_points
+    print (f"Piin likimääräinen arvo Monte Carlo methodilla on {pii:.4f}, {points_to_generate} genoroidun pisteen perusteella.")
