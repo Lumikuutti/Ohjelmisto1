c016431b3d31efad2c997978687c615b24e2118f
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-09-02 10:25:17 +0300

    Update

--- Moduuli5/Mod-5-Teht-4.py
+++ Moduuli5/Mod-5-Teht-4.py
@@ -1,15 +1,1 @@
-import random
-amount_to_throw = int( input("Anna kuinka monta arpakuutiota heitetään: "))
-if amount_to_throw < 0:
-    print("En voi heittää negatiivista määrää arpakuutiota.")
-else:
-    thrown = 0
-    added = 0
-    rolled = []
-    while thrown < amount_to_throw:
-        rolled.append(random.randint(1,6))
-        thrown = thrown + 1
-for roll in rolled:
-    added = added + roll
 
-print(f"Heitettyjen arpakuutioden arvojen summa on: {added}")
