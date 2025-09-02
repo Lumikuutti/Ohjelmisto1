a392f7126e97903ab89018252b29c5c63e33e28b
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-09-02 10:49:50 +0300

    Update

---
+++ Moduuli5/Mod-5-Teht-3.py
@@ -0,0 +1,6 @@
+new_number = input("Anna luku tai paina Enter ohjelman lopettamiseksi: ")
+numbers = []
+
+while new_number != "":
+    numbers.append(int(new_number))
+    new_number = input("Anna luku tai paina Enter ohjelman lopettamiseksi: ")
