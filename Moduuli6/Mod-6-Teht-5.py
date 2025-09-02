7e9c78ee87bb02c5f9ccd564abed6bfd7bf3a043
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-09-02 15:37:38 +0300

    Update

--- Moduuli6/Mod-6-Teht-5.py
+++ Moduuli6/Mod-6-Teht-5.py
@@ -1,11 +1,12 @@
 #definataan funktio
 def two_divisible(selection):
+    parilliset = []
     for number in selection:
-        if number % 2 != 0:
-            selection.remove(number)
-    return selection
+        if number % 2 == 0:
+            parilliset.append(number)
+    return parilliset
 
-numbers = [1, 4, 6, 8, 9, 3, 7, 2, 4]
+numbers = [1, 4, 6, 8, 9, 5, 3, 7, 2, 4]
 new_list = two_divisible(numbers)
 
 print(numbers)
