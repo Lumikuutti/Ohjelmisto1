f34f664dfdc1ca021bd5ce016c2646910bef185b
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-09-02 15:05:54 +0300

    Update

---
+++ Moduuli6/Mod-6-Teht-5.py
@@ -0,0 +1,11 @@
+#definataan funktio
+def two_divisible(selection):
+    for number in selection:
+        if number % 2 != 0:
+            selection.remove(number)
+    return selection
+
+numbers = [1, 4, 6, 8, 9, 3, 7, 2, 4]
+new_list = two_divisible(numbers)
+
+print(new_list)
