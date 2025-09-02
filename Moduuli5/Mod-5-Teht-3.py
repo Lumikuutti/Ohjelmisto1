61147267c82a3a63627f5937085b55f512d11d1b
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-09-02 11:42:30 +0300

    Update

--- Moduuli5/Mod-5-Teht-3.py
+++ Moduuli5/Mod-5-Teht-3.py
@@ -1,6 +1,18 @@
 new_number = input("Anna luku tai paina Enter ohjelman lopettamiseksi: ")
-numbers = []
+alkuluku = False
+if new_number == "":
+    print("Lopetit ohjelman heti.")
+else:
+    #muutetaan annettu luku integraaliksi
+    new_number = int(new_number)
+    #käydään läpi onko luku jaollinen muilla kuin itsellään
+    for x in range(2, new_number):
+        if new_number % x == 0:
+            alkuluku = False
+            print("Luku ei ole alkuluku.")
+            break
+        else:
+            alkuluku = True
 
-while new_number != "":
-    number = (int(new_number))
-    numbers.append(number)
+    if alkuluku:
+        print("Luku on alkuluku.")
