65b779fb9740b7430d27543bb3fc693797eb2d13
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-09-09 09:39:22 +0300

    Update

---
+++ Moduuli8/Mod-8-Teht-3.py
@@ -0,0 +1,4 @@
+seasons = ("Kevät", "Kesä", "Syksy", "Talvi")
+months = ("Tammi", "Helmi", "Maalis", "Huhti", "Touko", "Kesä", "Heinä", "Elo", "Syys", "Loka", "Marras", "Joulu")
+month = int( input("Anna kuukauden numero: "))
+
