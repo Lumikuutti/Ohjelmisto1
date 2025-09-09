8f8505d0105dd660ad6fc4965506b8e832f489df
Lumikuutti <227412132+Lumikuutti@users.noreply.github.com>
2025-09-09 21:28:05 +0300

    Update

---
+++ Moduuli8/Mod-8-Teht-2.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='stu',
+         password='qP72x[!-2}ELoh',
+         autocommit=True)
+
+def search_airports (country_code):
+    sql = "SELECT type, count(*) FROM airport WHERE iso_country = %s GROUP BY type;"
+    #print(sql)
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute (sql, (country_code,))
+    result = cursor.fetchall()
+    return result
+
+input_code = input("Anna maakoodi niin kerron sinulle kuinka monta minkäkin tyyppistä lentokenttää maasta löytyy: ")
+airports = search_airports(input_code)
+
+print(airports)
